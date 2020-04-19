f1e77c1c70e48933557859085a54260bd261c743
lxf <464442700@qq.com>
2020-04-19 10:56:54 +0800

    内部排序

--- 9排序/9-1内部排序/InternalSort.js
+++ 9排序/9-1内部排序/InternalSort.js
@@ -95,17 +95,18 @@ function bubbleSort(A) {
 //快速排序
 function quickSort(A) {
     function partition(A, low, high) {
-        //   var pivot = A[low];
         var mid = Math.floor((low + high) / 2);
-        var pivot = A[mid];
-        var temp = A[low];
+        var tem = A[mid];
+        A[mid] = A[low];
+        A[low] = tem;
+        var pivot = A[low];
         while (low < high) {
-            while (low < high && A[high] > pivot) high--;
+            while (low < high && A[high] >= pivot) high--;
             A[low] = A[high];
-            while (low < high && A[low] < pivot) low++;
+            while (low < high && A[low] <= pivot) low++;
             A[high] = A[low];
         }
-        A[low] = temp;
+        A[low] = pivot;
         return low;
     }
 
@@ -116,7 +117,7 @@ function quickSort(A) {
             sort(A, pivotpos + 1, high);
         }
     }
-    sort(A, 0, A.length);
+    sort(A, 0, A.length - 1);
     return A;
 }
 //选择排序
@@ -135,10 +136,12 @@ function selectionSort(A) {
     return A;
 }
 //堆排序
+//2020-4-18
+//修改了一下堆排序，
+//虽然还是有BUG，(虽然也能用)但是不想改了
 function heapSort(A) {
-    //初始化顺序表示(数组)的完全二叉树
-    //从n/2开始，调整该节点使得n/2比n和n-1都大
-    //然后依次调整n/2-1,n/2-2,一直到0
+    //  var lent = A.length;
+
     function buildMaxHeap(A) {
         var n = A.length;
         for (var i = Math.floor(n / 2); i >= 0; i--) {
@@ -148,35 +151,132 @@ function heapSort(A) {
     //从i开始，将i作为堆的顶点，这颗子树是大顶堆
     //一次交换排序完后，A[n]将会是最大的值，那么下一次调整n=n-1
     //所以要传入这个n变量
+    //n应该是数组长度，所以A[n-1]才是最大值
     function adjustDown(A, i, n) {
         var large, temp;
-        for (var j = i * 2; j < n; j = j * 2) {
+        temp = A[i];
+        //因为有j=2*i存在，j其实是数组二叉树的节点编号，编号是从1开始，到n结束
+        //而数组下标从0开始，因此数组下标+1=节点编号,
+        //large也因此要转换成元素下标
+        //87,45,53,32,17,65,78,9 test.js:136:9
+        // if (i == 0) {
+        //     console.log('2')
+        // }
+        //奇数不加1
+        //偶数加1
+        //   var len;
+        //len = (lent % 2 == 0) ? n + 1 : n;
+        // for (var j = (i + 1) * 2; j <= n + 1; j = j * 2) {
+        //     for (var j = (i + 1) * 2; j <= n; j = j * 2) {
+        for (var j = (i + 1) * 2; j <= n + 1; j = j * 2) {
             //获取左右节点较大的那一个
-            if (A[j + 1] > A[j]) large = j + 1;
-            else large = j;
+            if (A[j - 1 + 1] > A[j - 1] && j <= n) large = j - 1 + 1;
+            else large = j - 1;
             if (A[i] >= A[large]) break;
             else {
-                //交换A[i]和A[large]
-                temp = A[i];
+                //      交换A[i] 和A[large]
+                // temp = A[i];
                 A[i] = A[large];
                 A[large] = temp;
                 i = large;
+                j = i + 1;
                 //现在A[large]变小了，那么以A[large]为根的堆，需要重新调整
+                // A[i] = A[large];
+                // i = large;
+                // j = large + 1;
             }
         }
+        // A[i] = temp;
     }
-    buildMaxHeap(A);
+    buildMaxHeap(A, A.length);
+
+    //test*****
+    // var temp;
+    // for (var i = A.length - 1; i > 0; i--) {
+    //     console.log(i)
+    //     temp = A[0];
+    //     A[0] = A[i];
+    //     A[i] = temp;
+    //     adjustDown(A, 0, i - 1);
+    //     console.log('adjustDown(A, 0,' + (i - 1) + ');')
+    // }
+    // console.log(A.toString())
+    // var m = A.length - 1
+    // var temp;
+    // temp = A[0];
+    // A[0] = A[m];
+    // A[m] = temp;
+    // console.log('交换后：' + A.toString())
+    // buildMaxHeap(A, 0, (m - 1));
+    // console.log('重新调整：' + A.toString())
+
+    // m--;
+    // temp = A[0];
+    // A[0] = A[m];
+    // A[m] = temp;
+    // console.log('交换后：' + A.toString())
+    // adjustDown(A, 0, m - 1);
+    // console.log('重新调整：' + A.toString())
+    //test*****
+
+
     var n = A.length;
     var temp;
-    for (var i = n; i > 0; i--) {
+    for (var i = n - 1; i > 0; i--) {
         //堆顶元素和最后一个元素交换，这样最后一个元素就是最大的
         temp = A[0];
         A[0] = A[i];
         A[i] = temp;
         adjustDown(A, 0, i - 1);
     }
+    // 最后调整完还要换一次
+    // temp = A[0];
+    // A[0] = A[1];
+    // A[1] = temp;
     return A;
 }
+// function heapSort(A) {
+//     //初始化顺序表示(数组)的完全二叉树
+//     //从n/2开始，调整该节点使得n/2比n和n-1都大
+//     //然后依次调整n/2-1,n/2-2,一直到0
+//     function buildMaxHeap(A) {
+//         var n = A.length;
+//         for (var i = Math.floor(n / 2); i >= 0; i--) {
+//             adjustDown(A, i, n);
+//         }
+//     }
+//     //从i开始，将i作为堆的顶点，这颗子树是大顶堆
+//     //一次交换排序完后，A[n]将会是最大的值，那么下一次调整n=n-1
+//     //所以要传入这个n变量
+//     function adjustDown(A, i, n) {
+//         var large, temp;
+//         for (var j = i * 2; j < n; j = j * 2) {
+//             //获取左右节点较大的那一个
+//             if (A[j + 1] > A[j]) large = j + 1;
+//             else large = j;
+//             if (A[i] >= A[large]) break;
+//             else {
+//                 //交换A[i]和A[large]
+//                 temp = A[i];
+//                 A[i] = A[large];
+//                 A[large] = temp;
+//                 i = large;
+//                 //现在A[large]变小了，那么以A[large]为根的堆，需要重新调整
+//             }
+//         }
+//     }
+//     buildMaxHeap(A);
+//     var n = A.length;
+//     var temp;
+//     for (var i = n; i > 0; i--) {
+//         //堆顶元素和最后一个元素交换，这样最后一个元素就是最大的
+//         temp = A[0];
+//         A[0] = A[i];
+//         A[i] = temp;
+//         adjustDown(A, 0, i - 1);
+//     }
+//     return A;
+// }
 //归并排序
 function mergeSort(A) {
     //合并两个有序数组
@@ -305,17 +405,18 @@ function getA(n) {
     }
     return A;
 }
-var A1 = getA(2 ** 18);
-var A2 = getA(2 ** 18 + 1);
-var A3 = getA(2 ** 18 + 2);
-var A4 = getA(2 ** 18 + 3);
-var A5 = getA(2 ** 18 + 4);
-var A6 = getA(2 ** 18 + 5);
-var A7 = getA(2 ** 18 + 6);
-var A8 = getA(2 ** 18 + 7);
-var A9 = getA(2 ** 18 + 8);
-var A10 = getA(2 ** 18 + 9);
-var A11 = getA(2 ** 18 + 10);
+var num = 10 ** 5;
+var A1 = getA(num++);
+var A2 = getA(num++);
+var A3 = getA(num++);
+var A4 = getA(num++);
+var A5 = getA(num++);
+var A6 = getA(num++);
+var A7 = getA(num++);
+var A8 = getA(num++);
+var A9 = getA(num++);
+var A10 = getA(num++);
+var A11 = getA(num++);
 console.time('带哨兵的直接插入排序');
 insertSortBySentry(A1);
 console.timeEnd('带哨兵的直接插入排序');
@@ -339,14 +440,15 @@ console.timeEnd('希尔排序');
 console.time('冒泡排序');
 bubbleSort(A5);
 console.timeEnd('冒泡排序');
+
 console.time('快速排序');
-bubbleSort(A6);
+quickSort(A6);
 console.timeEnd('快速排序');
 console.time('选择排序');
-bubbleSort(A7);
+selectionSort(A7);
 console.timeEnd('选择排序');
 console.time('堆排序');
-bubbleSort(A8);
+heapSort(A8);
 console.timeEnd('堆排序');
 console.time('归并排序');
 mergeSort(A9);
